e4c796939affccd4ebefc1a8848ca75966b52c3d
leqiuhong <oleolema@qq.com>
2025-06-01 10:58:36 +0800

    1

--- src/test/test_server.py
+++ src/test/test_server.py
@@ -1,7 +1,13 @@
-from mcp_server_ds.server import ScriptRunner
+from mcp_server_ds.script_runner import ScriptRunner
 
 
 if __name__ == '__main__':
     script_runner = ScriptRunner()
-    script_runner.load_csv("/Users/leqiuhong/PycharmProjects/langchain-mcp-adapters/tests/mock/sale_table.csv")
-    result = script_runner.safe_eval("# 提取京东的销量数据\njd_sales = df_1[df_1['二级品类'] == '京东']['销量'].sum()\nprint(jd_sales)")  # 捕获 safe_eval 的返回值
+    result = script_runner.safe_eval(
+        script="print(df_1.head())"
+    )  # 捕获 safe_eval 的返回值
+    for item in result:
+        if hasattr(item, 'text'):
+            print(item.text)  # 打印 safe_eval 的返回值
+        else:
+            print(item)
